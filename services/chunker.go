f87728733b63228e14ed891d9d6cc2ab96e020cb
lmbangel <mbangelalihle@gmail.com>
2026-06-30 23:00:35 +0200

    -- completed the chunking operation on the chunking funtion

--- services/chunker.go
+++ services/chunker.go
@@ -1,5 +1,7 @@
 package services
 
+import "fmt"
+
 type Chunk struct {
 	ID     string
 	Text   string
@@ -13,5 +15,29 @@ func ChunkText(text string, source string, chunkSize int, overlap int) []Chunk {
 	if len == 0 {
 		return cs
 	}
+	start := 0
+	chunkIndex := 0
+
+	for start < len {
+		end := start + chunkSize
+		if end > len {
+			end = len
+		}
+
+		c := string(r[start:end])
+		cs = append(cs, Chunk{
+			ID:     fmt.Sprintf("%s-chunk-%d", source, chunkIndex),
+			Text:   c,
+			Source: source,
+		})
+
+		chunkIndex++
+		start = start + (chunkSize - overlap)
+
+		if start >= len {
+			break
+		}
+	}
+
 	return cs
 }
